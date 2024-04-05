77abe25406d1e24fcee9e35b168a3ea7f9c4c2f8
zoeleca <zoennts@gmail.com>
2024-04-05 19:07:22 +0200

    board component created

---
+++ src/components/Game/Board.jsx
@@ -0,0 +1,36 @@
+import Square from "./Square";
+const Board = ({ squares, handleClick }) => {
+  return (
+    <div className="board">
+      <div className="board-row">
+        {[0, 1, 2].map((i) => (
+          <Square
+            key={i}
+            value={squares[i]}
+            onSquareClick={() => handleClick(i)}
+          />
+        ))}
+      </div>
+      <div className="board-row">
+        {[3, 4, 5].map((i) => (
+          <Square
+            key={i}
+            value={squares[i]}
+            onSquareClick={() => handleClick(i)}
+          />
+        ))}
+      </div>
+      <div className="board-row">
+        {[6, 7, 8].map((i) => (
+          <Square
+            key={i}
+            value={squares[i]}
+            onSquareClick={() => handleClick(i)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
