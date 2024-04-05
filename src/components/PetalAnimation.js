ac5b306fd547b44f62d965266ccb116764a9ae7c
zoeleca <zoennts@gmail.com>
2024-04-05 15:15:30 +0200

    animation log

--- src/components/PetalAnimation.js
+++ src/components/PetalAnimation.js
@@ -1,20 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const PetalAnimation = () => {
   useEffect(() => {
-    const canvas = document.createElement('canvas');
+    const canvas = document.createElement("canvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     document.body.appendChild(canvas); // Append canvas to the body
 
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext("2d");
 
     const TOTAL = 100;
     let petalArray = [];
 
     const petalImg = new Image();
-    petalImg.src = './src/assets/petal.png';
-    petalImg.addEventListener('load', () => {
+    petalImg.src = "./src/assets/petal.png";
+    petalImg.addEventListener("load", () => {
       generatePetals();
       animate();
     });
@@ -27,6 +27,7 @@ const PetalAnimation = () => {
     }
 
     function animate() {
+      console.log('The animation has started')
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       petalArray.forEach((petal) => petal.animate());
       requestAnimationFrame(animate);
@@ -38,7 +39,7 @@ const PetalAnimation = () => {
       generatePetals();
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     // Petal class
     class Petal {
@@ -48,7 +49,7 @@ const PetalAnimation = () => {
 
       reset() {
         this.x = Math.random() * canvas.width;
-        this.y = (Math.random() * canvas.height * 2) - canvas.height;
+        this.y = Math.random() * canvas.height * 2 - canvas.height;
         this.w = 25 + Math.random() * 15;
         this.h = 20 + Math.random() * 10;
         this.opacity = this.w / 40;
@@ -65,8 +66,8 @@ const PetalAnimation = () => {
           petalImg,
           this.x,
           this.y,
-          this.w * (0.6 + (Math.abs(Math.cos(this.flip)) / 3)),
-          this.h * (0.8 + (Math.abs(Math.sin(this.flip)) / 5))
+          this.w * (0.6 + Math.abs(Math.cos(this.flip)) / 3),
+          this.h * (0.8 + Math.abs(Math.sin(this.flip)) / 5)
         );
       }
 
@@ -84,14 +85,14 @@ const PetalAnimation = () => {
     }
 
     // CSS
-    canvas.style.position = 'absolute';
-    canvas.style.top = '0';
-    canvas.style.left = '0';
-    canvas.style.zIndex = '-1'; //
+    canvas.style.position = "absolute";
+    canvas.style.top = "0";
+    canvas.style.left = "0";
+    canvas.style.zIndex = "-1"; //
 
     // Cleanup function
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
       document.body.removeChild(canvas); // Remove canvas from the body
     };
   }, []); // Empty dependency array means it runs once on mount
